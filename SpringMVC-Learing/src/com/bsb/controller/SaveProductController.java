e74715f515ae480224ce8c07c49e25095ca0a79a
challengerZeng <664903471@qq.com>
2018-03-08 00:18:44 +0800

    使用spring框架的DispatcherServlet

--- SpringMVC-Learing/src/com/bsb/controller/SaveProductController.java
+++ SpringMVC-Learing/src/com/bsb/controller/SaveProductController.java
@@ -2,42 +2,46 @@ package com.bsb.controller;
 
 import com.bsb.domain.Product;
 import com.bsb.form.ProductForm;
-import com.bsb.validator.ProductValidator;
+import org.apache.commons.logging.LogFactory;
+import org.apache.commons.logging.Log;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.List;
 
 public class SaveProductController implements Controller {
-    @Override
-    public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
 
+    private static final Log logger = LogFactory.getLog(SaveProductController.class);
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
+        logger.info("SaveProductController called");
         ProductForm productForm = new ProductForm();
 
+        //populate action properties
         productForm.setName(request.getParameter("name"));
         productForm.setDesc(request.getParameter("desc"));
         productForm.setPrice(request.getParameter("price"));
 
-        //validate ProductForm
-        ProductValidator productValidator = new ProductValidator();
-        List<String> errors = productValidator.validate(productForm);
-        if (errors.isEmpty()) {
-            //create model
-            Product product = new Product();
-            product.setName(productForm.getName());
-            product.setDesc(productForm.getDesc());
+        //create model
+        Product product = new Product();
+        product.setName(productForm.getName());
+        product.setDesc(productForm.getDesc());
+        try {
             product.setPrice(Float.parseFloat(productForm.getPrice()));
-            //save product
-            request.setAttribute("product", product);
-            System.out.println("errors isEmpty added");
-            return "/WEB-INF/jsp/ProductDetails.jsp";
-        } else {
-            //store errors and form in a scope variable for the view
-            request.setAttribute("errors", errors);
-            request.setAttribute("form", productForm);
-            System.out.println("had some errors added");
-            return "/WEB-INF/jsp/ProductForm.jsp";
-        }
+        } catch (NumberFormatException ex) {}
 
+        //insert code to save product
+
+
+//        request.setAttribute("product", product);
+//        System.out.println("errors isEmpty added");
+//        //store errors and form in a scope variable for the view
+//        request.setAttribute("form", productForm);
+//        System.out.println("had some errors added");
+//        return null;
+        return new ModelAndView("WEB-INF/jsp/ProductDetails.jsp", "product", product);
     }
+
 }
