227a7ef210d01a579bbcc31229ec26ba8725c6e8
challengerZeng <664903471@qq.com>
2018-03-24 19:24:23 +0800

    测试

---
+++ MobileChatServer/src/main/java/com/bsb/utils/ImageUtil/ImagesUtil.java
@@ -0,0 +1,10 @@
+package com.bsb.utils.ImageUtil;
+
+public class ImagesUtil {
+
+    private static final String defaultImagePath = "C:\\Users\\66490\\Desktop\\user_image\\defaultImage";
+    private static final String imageSavePath = "C:\\Users\\66490\\Desktop\\user_image\\";
+
+
+    
+}
